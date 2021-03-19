35ae8fad323ad926a428682b376ed911b3fcbdb3
Danilo Ibañez <danilobepo@gmail.com>
2021-03-19 16:10:10 -0300

    Change of visual

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,6 @@
 import React , { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions , StyleSheet, Text, View, Image, ScrollView, ImageBackground } from 'react-native';
+import { Dimensions , StyleSheet, Text, View, Image, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
 import {Picker} from '@react-native-community/picker';
 import data from "../data/characters-info.json";
 import vision from "../data/vision.json";
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     flexWrap: 'wrap',
-    backgroundColor: '#7d0ee6',
+    backgroundColor: '#fff',
   },
   image: {
     width: Dimensions.get('window').width*0.08,
@@ -41,49 +41,28 @@ const styles = StyleSheet.create({
 });
 
 export default function Home() {
-  const getIcons = data.map( (data) => {
-    return (
-      <ImageBackground 
-        key = {data.name}
-        source={{
-          uri: data.img,
-        }}
-        style={styles.imagen}
-      >
-        <Image
-          style={styles.image}
-          source={{
-            uri: "https://ih1.redbubble.net/image.1849347856.1942/st,small,507x507-pad,600x600,f8f8f8.jpg",
-          }}
-        />
-      </ImageBackground>
-    )
-  });
 
-  const [icons, setIcons] = useState(getIcons);
-
-
+  const [icons, setIcons] = useState([]);
   const [star, setStar] = useState(0);
   const [element, setElement] = useState("All");
 
   const setStarFunction = (value) => {
     setStar(value);
   };
-
   const setElemFunction = (value) => {
     setElement(value);
   };
 
-
+  // React function used to handle changes in characters depending on the filters used
   React.useEffect(() => {
     const applyFilter = () => {
-      if(star == 0 && element == 0){
-        setIcons(getIcons);
+      if(star === 0 && element === "All"){
+        setIcons(getIconsFilter([], true));
       }else{
         let auxStar = filterStar(star);
         let auxElement = filterElement(element);
         let characters = auxStar.filter(x => auxElement.includes(x));
-        setIcons(getIconsFilter(characters));
+        setIcons(getIconsFilter(characters, false));
       }
     };
     const filterStar = (option) => {
@@ -108,10 +87,11 @@ export default function Home() {
       });
       return aux;
     }
-    const getIconsFilter = (characters) => {
+    // Params -> Characters: list of characters to draw, option: true is to draw all characters, false is to draw only the characters in the list
+    const getIconsFilter = (characters, option) => {
       let aux = [];
       data.map( (data) => {
-        if(characters.includes(data.name)){
+        if(characters.includes(data.name) || option){
           var icon =  data.vision === "Pyro" ? require("../data/vision/Pyro.png") :
                       data.vision === "Hydro" ? require("../data/vision/Hydro.png") :
                       data.vision === "Electro" ? require("../data/vision/Electro.png") :
@@ -121,26 +101,28 @@ export default function Home() {
                       data.vision === "Dendro" ? require("../data/vision/Dendro.png") :
                       require("../data/vision/Error.png");
           aux.push(
-            <ImageBackground 
-              key = {data.name}
-              source={{
-                uri: data.img,
-              }}
-              style={styles.imagen}
-            >
-              <Image
-                style={styles.image}
-                source={icon}
-              />
-            </ImageBackground>
+            <TouchableOpacity key = {data.name} onPress={() => {console.log(data.name)}}>
+              <ImageBackground
+                source={{
+                  uri: data.img,
+                }}
+                style={styles.imagen}
+              >
+                <Image
+                  style={styles.image}
+                  source={icon}
+                />
+              </ImageBackground>
+            </TouchableOpacity>
           );
         }
       });
       return aux;
     }
     applyFilter();
-  }, [star, element]);
+  }, [star, element]); // If these variables change, the function is executed
 
+  // Widget of the filters characters app
   const filter = () =>{
     return (
       <View style={styles.filterView}>
@@ -149,9 +131,9 @@ export default function Home() {
           style={{ height: 35, width: 150}}
           onValueChange={(itemValue, itemIndex) => setStarFunction(itemValue)}
         >
-          <Picker.Item label="All start" value={0} />
-          <Picker.Item label="5 start" value={5} />
-          <Picker.Item label="4 start" value={4} />
+          <Picker.Item label="All star" value={0} />
+          <Picker.Item label="5 star" value={5} />
+          <Picker.Item label="4 star" value={4} />
         </Picker>
 
         <Picker
@@ -179,7 +161,7 @@ export default function Home() {
         <View style={styles.view}>
         {
           icons.length > 0 ? icons :
-          <Text>No hay elementos!</Text>
+          <Text>There are no characters!</Text>
         }
         </View>
       </ScrollView>
